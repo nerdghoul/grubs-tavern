15e2c18edf434c974ed35793a743b25556c96350
toomanybooks <benjtibbetts@gmail.com>
2018-03-12 19:25:48 -0400

    show parameter example in routing

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,8 @@ from flask import  render_template
 def index():
     return "Hello, World!"
 
+# <name> is an example of how to add parameters, here it is ignored
+@app.route('/oafishdistortpurp/<name>')
 @app.route('/oafishdistortpurp')
 def oafishdistortpurp(name=None):
     return render_template('oafishdistortpurp.html',name=name)
