368c8626f3e702b36b4f91ba8bf5041cc84b22dd
Sam Patterson <patterson.samuel.s@gmail.com>
2018-03-22 13:41:11 -0400

    Adjusting / and /index to return index.html instead of returning "Hello world!"
    Also adding a route to phaserlanding.html

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,14 @@ from flask import  render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 # <name> is an example of how to add parameters, here it is ignored
 @app.route('/oafishdistortpurp/<name>')
 @app.route('/oafishdistortpurp')
 def oafishdistortpurp(name=None):
     return render_template('oafishdistortpurp.html',name=name)
+
+@app.route('/phaserlanding')
+def phaserlanding():
+    return render_template('phasertemplate.html')
